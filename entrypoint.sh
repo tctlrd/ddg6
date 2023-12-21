0fcd1fb2ca3107026d4531f077336e98fafbdbec
tctlrd <135671403+tctlrd@users.noreply.github.com>
2023-12-21 08:26:53 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -42,8 +42,7 @@ for USER in $(ls -1 /home); do
     chown "$host_uid:$host_gid" "$LOG"
     chmod 644 "$LOG"
     # first fetch
-    su $USER -c "getmail --rcfile="$RC" 2>&1 | tee "$LOG""
-    echo "su $USER -c "getmail --rcfile="$RC" 2>&1 | tee "$LOG"""
+    su $USER -c "getmail --rcfile=\"$RC\""
     echo "Running first mail fetch for $ACC as $USER with getmail."
   done
 done
