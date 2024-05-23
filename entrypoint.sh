094fcb491c5c2c4ceb2fc4e20f99cee38915cb11
tctlrd <135671403+tctlrd@users.noreply.github.com>
2024-05-23 15:08:11 +0000

    Update entrypoint.sh
    initial cron job execution on container start

--- entrypoint.sh
+++ entrypoint.sh
@@ -46,6 +46,7 @@ done
 echo "Starting services..."
 /etc/init.d/dovecot start
 /etc/init.d/cron start
+run-parts /etc/cron.d
 
 tail -F /var/log/dovecot/dovecot.log ""/var/log/getmail/*.log""
 exec "$@"
